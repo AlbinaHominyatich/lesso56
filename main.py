efb7ac1ca9b6eeb1b3df49ef77a648c8ba7eb39c
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-11-05 17:48:43 +0200

    про батьків`

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ class Child(Parents):
         print(self.height)
         print(self.speed)
 max_roma_pavlo = Grandparents()
+#lesson6
+import requests
+help(requests)
+rq = requests
+print("for Korsun ",requests.__cake__)
+print(rq.__name__)
+print(rq.__url__)
+print(type(3))
+ls = []
+for i in dir(ls):
+    print(i)
+print(dir(ls))
